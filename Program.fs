3fcbc10e08e46a6a42fe79c9f1ccc1a72ff7c620
Andrew Sutton <andrewsydsutton@gmail.com>
2023-06-06 17:32:12 -0500

    First commit

---
+++ Program.fs
@@ -0,0 +1,7 @@
+﻿// For more information see https://aka.ms/fsharp-console-apps
+printfn "Hello from F#"
+
+let p = new Microsoft.Data.SqlClient.SqlParameter("@p1", new System.DateOnly(1977,1,1))
+
+printfn $"{p.DbType}"
+printfn $"{p.SqlDbType}"
